b492b576abaabeaf0163098f05d55202f3d5be48
debojyoti-basu <39114373+debojyoti-basu@users.noreply.github.com>
2018-05-09 12:11:38 +0530

    Update file1.cpp

--- file1.cpp
+++ file1.cpp
@@ -1,3 +1,5 @@
 // The File is the part of the project for Image based Localization for Self Driving Vehicles.
 //Reference to the Challenge for the developing image based localization and not rely solely on LIDAR or GPS
 //for navigating the self dribving vehicle.
+//Image Processing Libraries like OpenCV needs to be used for processing the data frame acquired from
+//image sensors.
